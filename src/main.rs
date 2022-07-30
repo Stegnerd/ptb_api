e38761f6c79b003fc8c764c1d7943fd106b86c18
Dalton Stegner <dmstegner@gmail.com>
2022-07-30 19:34:51 -0400

    connection works

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ extern crate rocket;
 extern crate diesel;
 extern crate dotenv;
 
+use diesel::{EqAll, QueryDsl, RunQueryDsl};
+use ptb_api::establish_connection;
+use ptb_api::models::User;
+use ptb_api::schema::users::dsl::users;
+use ptb_api::schema::users::email;
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
@@ -16,6 +22,15 @@ fn ping() -> &'static str {
 #[launch]
 fn rocket() -> _ {
     println!("starting up the api");
+
+    let connection = establish_connection();
+    let results = users
+        .limit(5)
+        .load::<User>(&connection)
+        .expect("Error loading users");
+
+    println!("Displaying {} users", results.len());
+
 
     rocket::build()
         .mount("/", routes![index, ping])
