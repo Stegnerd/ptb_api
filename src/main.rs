2aeeebe7aac653954d61d78bc1742023f36eed0a
Dalton Stegner <dmstegner@gmail.com>
2022-07-31 02:11:21 -0400

    migrations work

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,17 @@
 extern crate rocket;
 extern crate diesel;
 extern crate dotenv;
+#[macro_use]
+extern crate diesel_migrations;
 
+use diesel_migrations::{embed_migrations};
 use diesel::{ QueryDsl, RunQueryDsl};
-use ptb_api::configuration::get_configuration;
+use ptb_api::configuration::{get_configuration};
 use ptb_api::models::User;
 use ptb_api::schema::users::dsl::users;
+
+// creates module for referencing migrations so you can put them in your binary
+embed_migrations!();
 
 #[get("/")]
 fn index() -> &'static str {
@@ -24,13 +30,18 @@ fn rocket() -> _ {
 
     // let connection = establish_connection();
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = &configuration.database.get_connection();
+    // This will run the necessary migrations.
+    let answer = embedded_migrations::run(connection);
+
+
+    //run_migrations(configuration);
     let results = users
         .limit(5)
-        .load::<User>(&configuration.database.get_connection())
+        .load::<User>(connection)
         .expect("Error loading users");
 
     println!("Displaying {} users", results.len());
-
 
     rocket::build()
         .mount("/", routes![index, ping])
